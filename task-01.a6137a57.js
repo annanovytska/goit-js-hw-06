e22ae169f1c29c78fcfcef35bebae5ea3b814bc3
annanovytska <134167039+annanovytska@users.noreply.github.com>
2023-08-25 17:02:26 +0000

    Deploying to gh-pages from @ annanovytska/goit-js-hw-06@e67b4f946e990fe1e83a4461e0fef043d2f0a5df 🚀

---
+++ task-01.a6137a57.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#categories").children;console.log(`Number of categories: ${e.length}`);[...e].forEach((e=>{console.log(`Category: ${e.firstElementChild.textContent}`),console.log(`Elements: ${e.lastElementChild.children.length}`)}));
+//# sourceMappingURL=task-01.a6137a57.js.map
