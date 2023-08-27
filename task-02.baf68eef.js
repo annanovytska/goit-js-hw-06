575782565dbe39557b10da09a69af3ad9a4828ca
annanovytska <134167039+annanovytska@users.noreply.github.com>
2023-08-27 11:56:16 +0000

    Deploying to gh-pages from @ annanovytska/goit-js-hw-06@68861d60b9167010a4254ecc0bef1b9d3e8682d2 🚀

---
+++ task-02.baf68eef.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("#ingredients");const e=["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"].map((t=>function(t){const e=document.createElement("li");return e.classList.add("item"),e.textContent=t,e}(t)));t.append(...e);
+//# sourceMappingURL=task-02.baf68eef.js.map
