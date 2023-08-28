d6549de0c1f8c60cd162b8c91c29aa6adede4e9b
annanovytska <134167039+annanovytska@users.noreply.github.com>
2023-08-28 13:53:31 +0000

    Deploying to gh-pages from @ annanovytska/goit-js-hw-06@4663ae980f2d884e7c0ae7d630358792374e7667 🚀

---
+++ task-04.2937de10.js
@@ -0,0 +1,2 @@
+let t=0;const e=document.querySelector("#value");document.querySelector('button[data-action="decrement"]').addEventListener("click",(function(n){t-=1,e.textContent=t}));document.querySelector('button[data-action="increment"]').addEventListener("click",(function(n){t+=1,e.textContent=t}));
+//# sourceMappingURL=task-04.2937de10.js.map
---
+++ task-04.37bb4288.js
@@ -0,0 +1,2 @@
+!function(){var t=0,e=document.querySelector("#value");document.querySelector('button[data-action="decrement"]').addEventListener("click",(function(n){t-=1,e.textContent=t})),document.querySelector('button[data-action="increment"]').addEventListener("click",(function(n){t+=1,e.textContent=t}))}();
+//# sourceMappingURL=task-04.37bb4288.js.map
