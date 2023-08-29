c3d505edfb54393b65259c7621aa9c0b60257917
annanovytska <134167039+annanovytska@users.noreply.github.com>
2023-08-29 10:52:54 +0000

    Deploying to gh-pages from @ annanovytska/goit-js-hw-06@cd8a324157a80794661b1e9a7e7703656f24c583 🚀

---
+++ task-07.4e5596be.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#font-size-control"),t=document.querySelector("#text");e.addEventListener("input",(function(n){var o=e.value;t.style.fontSize="".concat(o,"px")})),console.dir(e)}();
+//# sourceMappingURL=task-07.4e5596be.js.map
---
+++ task-07.c6c16fc1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#font-size-control"),t=document.querySelector("#text");e.addEventListener("input",(function(n){const o=e.value;t.style.fontSize=`${o}px`})),console.dir(e);
+//# sourceMappingURL=task-07.c6c16fc1.js.map
