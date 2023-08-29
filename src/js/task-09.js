0f80639c039785fdd6639efc7fb156b699783fd0
Anna <annanovytskat@gmail.com>
2023-08-29 19:24:57 +0100

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonItem = document.querySelector('.change-color');
+const spanItem = document.querySelector('.color');
+
+buttonItem.addEventListener('click', onButtonItemClick);
+function onButtonItemClick(event) {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  document.body.dataset.backgroundColor = color;
+  // const bgColor = document.body.style.backgroundColor;
+  const bgColor = document.body.dataset.backgroundColor;
+  console.log(bgColor);
+  spanItem.textContent = bgColor;
+}
