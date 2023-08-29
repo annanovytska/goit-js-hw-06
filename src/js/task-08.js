699ca96c4af2a3168252c97d99de48ea97a80ea2
Anna <annanovytskat@gmail.com>
2023-08-29 18:43:58 +0100

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const formElement = document.querySelector('.login-form');
+console.dir(formElement);
 
+formElement.addEventListener('submit', onFormElementSubmit);
+
+function onFormElementSubmit(event) {
+  event.preventDefault();
+  const email = formElement.elements.email.value;
+  const password = formElement.elements.password.value;
+  if (email === '' || password === '') {
+    alert('Всі поля повинні бути заповненні');
+  } else {
+    const saveUserInfo = { email, password };
+    formElement.reset();
+  }
+}
